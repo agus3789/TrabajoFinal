6d83ea1ac98ca10c2e0cc978deb94b3852293f08
agus3789 <65677650+agus3789@users.noreply.github.com>
2021-02-17 21:10:55 -0300

    Add files via upload

--- routes/bd.js
+++ routes/bd.js
@@ -1,11 +1,22 @@
 var mysql=require('mysql');
 
-var conexion=mysql.createConnection({
-    host:'remotemysql.com',
-    user:'35S0HfTPFE',
-    password:'18HwyinR26',
-    database:'35S0HfTPFE'
+const {createPool} = require("mysql");
+
+const pool = createPool({
+    host     : 'remotemysql.com',
+    user     : '35S0HfTPFE',
+    password : '18HwyinR26',
+    database : '35S0HfTPFE'
 });
+
+module.exports = pool;
+var conexion=mysql.createConnection({
+   host:'remotemysql.com', 
+   user:'35S0HfTPFE', 
+   password:'18HwyinR26', 
+   database:'35S0HfTPFE'
+});
+
 
 conexion.connect(function (error){
     if (error)
